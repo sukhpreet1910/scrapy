4541d07e84d7fe80ead99c5340fd1721aa75aab6
Sukh Sodhi <sukh00422@gmail.com>
2024-03-28 13:43:04 -0700

    building item

--- myscraper/myscraper/items.py
+++ myscraper/myscraper/items.py
@@ -6,7 +6,19 @@
 import scrapy
 
 
-class MyscraperItem(scrapy.Item):
+class BookItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    name = scrapy.Field(),
+    url = scrapy.Field(),
+    price = scrapy.Field()
+    description = scrapy.Field()
+    upc = scrapy.Field()
+    product_type = scrapy.Field()
+    price_excl_tax = scrapy.Field()
+    price_incl_tax = scrapy.Field()
+    tax = scrapy.Field()
+    availability = scrapy.Field()
+    number_od_reviews = scrapy.Field()
+    category = scrapy.Field()
+
--- myscraper/myscraper/spiders/bookspider.py
+++ myscraper/myscraper/spiders/bookspider.py
@@ -1,5 +1,5 @@
 import scrapy
-
+from myscraper.items import BookItem  
 
 class BookspiderSpider(scrapy.Spider):
     name = "bookspider"
@@ -12,19 +12,22 @@ class BookspiderSpider(scrapy.Spider):
             product_page_url = book.css('h3 a').attrib['href']
             
             if 'catalogue/' in product_page_url:
-                book_url = "https://books.toscrape.com" + str(product_page_url)
+                book_url = "https://books.toscrape.com/" + product_page_url
+                print(book_url)
             else:
-                book_url = "https://books.toscrape.com/catalogue/" + str(product_page_url)
+                book_url = "https://books.toscrape.com/catalogue/" + product_page_url
+                print(book_url)
+
 
             yield scrapy.Request(book_url, callback=self.parse_book_page)
 
-        next_page = response.css('li.next a ::attr(href)').get()
+        next_page = str(response.css('li.next a ::attr(href)').get())
 
-        if next_page:
+        if next_page is not None:
             if 'catalogue/' in next_page:
-                next_page_url = "https://books.toscrape.com" + str(next_page)
+                next_page_url = "https://books.toscrape.com/" + next_page
             else:
-                next_page_url = "https://books.toscrape.com/catalogue/" + str(next_page)
+                next_page_url = "https://books.toscrape.com/catalogue/" + next_page
             yield response.follow(next_page_url, callback=self.parse)
 
     
@@ -32,35 +35,19 @@ class BookspiderSpider(scrapy.Spider):
     def parse_book_page(self, response):
         book = response.css('div.product_main')[0]
         table_rows = response.css('table tr')
+        book_item = BookItem()
 
-        yield{
-            'name' : book.css('h1 ::text').get(),
-            'url' : response.url,
-            'price' : book.css('p.price_color ::text').get(),
-            'description' : book.xpath("//div[@id='product_description']/following-sibling::p/text()").get(),
-            'upc' : table_rows[0].css('td ::text').get(),
-            'Product Type' : table_rows[1].css('td ::text').get(),
-            'Price (excl. tax)' : table_rows[2].css('td ::text').get(),
-            'Price (incl. tax)' : table_rows[3].css('td ::text').get(),
-            'Tax' : table_rows[4].css('td ::text').get(),
-            'Availability' : table_rows[5].css('td ::text').get(),
-            'Number of reviews' : table_rows[6].css('td ::text').get(),
-            'Category' : book.xpath('//ul[@class="breadcrumb"]/li[@class="active"]/preceding-sibling::li[1]/a/text()').get()
-        }
-            
+        book_item['name'] = book.css('h1 ::text').get(),
+        book_item['url' ]= response.url,
+        book_item['price'] = book.css('p.price_color ::text').get(),
+        book_item['description'] = book.xpath("//div[@id='product_description']/following-sibling::p/text()").get(),
+        book_item['upc'] = table_rows[0].css('td ::text').get(),
+        book_item['Product Type'] = table_rows[1].css('td ::text').get(),
+        book_item['price_excl_tax)'] = table_rows[2].css('td ::text').get(),
+        book_item['price_incl_tax)'] = table_rows[3].css('td ::text').get(),
+        book_item['tax'] = table_rows[4].css('td ::text').get(),
+        book_item['availability'] = table_rows[5].css('td ::text').get(),
+        book_item['number_of_reviews'] = table_rows[6].css('td ::text').get(),
+        book_item['category'] = book.xpath('//ul[@class="breadcrumb"]/li[@class="active"]/preceding-sibling::li[1]/a/text()').get()
 
-        
-        next_url = response.css('li.next a ::attr(href)').get()
-    
-        if next_url is not None:
-            if 'catalogue/' in next_url:
-                # next_page_url = f"https://books.toscrape.com/{next_url}"
-                next_page_url = "https://books.toscrape.com/" + str(next_url)
-
-            else:
-                # next_page_url = f"https://books.toscrape.com/catalogue/{next_url}"
-                next_page_url = "https://books.toscrape.com/catalogue/" + str(next_url)
-
-            yield response.follow(next_page_url, callback=self.parse)
-        
-
+        yield book_item
